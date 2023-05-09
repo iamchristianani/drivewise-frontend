5756f3ecc6ca91e8b9fd823216cd9d6d71d7a7c5
Christian Ani <iamchristianani@gmail.com>
2023-05-09 15:22:38 +0100

    Update redux to store and retrieve cars

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import authReducer from './authSlice';
+import carsReducer from './cars/cars';
 
-const store = configureStore({
-  reducer: {
-    authentications: authReducer,
+const store = configureStore(
+  {
+    reducer: {
+      authentications: authReducer,
+      cars: carsReducer,
+    },
   },
-});
+  applyMiddleware(thunk),
+);
 
 export default store;
