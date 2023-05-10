561ef08b5ecc6eda8ce742f0d4474ff8f3962f78
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2023-05-10 17:00:25 +0300

    fix navbar with login

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import RegisterWithUsername from './components/RegisterWithUsername';
-import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import ReservationForm from './pages/ReservationForm';
 
 function App() {
   return (
     <main className="App">
       <Router>
-        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<RegisterWithUsername />} />
           <Route path="/view_all_tests" element="view all tests" />
-          <Route path="/reservation_form" element="reservation form" />
+          <Route path="/reservation_form" element={<ReservationForm />} />
           <Route path="/my_reservations" element="my reservations" />
         </Routes>
       </Router>
